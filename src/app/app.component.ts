15b00acbcf1467e69c0ed8486f49ae4fc66e4004
ebakunin <ebakunin@gmail.com>
2021-07-22 18:10:16 -0700

    Refactored component. Moved explanation functionality to its own component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import {
-    ChangeDetectionStrategy,
-    Component,
-    ElementRef,
-    QueryList,
-    Renderer2,
-    ViewChildren
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { merge } from 'rxjs';
 import { startOfToday } from 'date-fns';
 
+import { AppService } from './services/app.service';
 import { EmployeeService } from './services/employee.service';
 import { OfficeService } from './services/office.service';
 import { SpinnerService } from './services/spinner.service';
@@ -15,48 +10,22 @@ import { SpinnerService } from './services/spinner.service';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-    @ViewChildren('showSteps') showSteps!: QueryList<ElementRef>;
-
-    public readonly showSpinner$ = this._spinnerService.showSpinner$;
+    public readonly showMesh$ = merge(this._spinnerService.showSpinner$, this._appService.showExplanation$);
     public readonly employee$ = this._employeeService.employees.first$;
     public readonly office$ = this._officeService.offices.first$;
 
     public newExceptionDates: Date[] = [];
     public exceptionDatesToBeDeleted: Date[] = [];
     public startingViewDate = startOfToday();
-    public showMesh = false;
 
-    constructor(private _employeeService: EmployeeService,
+    constructor(private _appService: AppService,
+                private _employeeService: EmployeeService,
                 private _officeService: OfficeService,
-                private _renderer: Renderer2,
                 private _spinnerService: SpinnerService) {}
-
-    /**
-     *
-     */
-    public onShowExplanationText(): void {
-        this.showMesh = true;
-        this._renderer.setStyle(this.showSteps.get(0)?.nativeElement, 'display', 'block');
-    }
-
-    /**
-     * @param {number | "end"} step
-     */
-    public showNextExplanationText(step: number | 'end'): void {
-        this.showSteps.forEach(el => {
-            this._renderer.setStyle(el.nativeElement, 'display', 'none');
-        });
-
-        if (step !== 'end') {
-            this._renderer.setStyle(this.showSteps.get(step)?.nativeElement, 'display', 'block');
-        } else {
-            this.showMesh = false;
-        }
-    }
 
     /**
      *
