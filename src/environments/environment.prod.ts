193504ad6839fd8d58b160592d61b2e598048e1b
ebakunin <ebakunin@gmail.com>
2021-07-22 18:08:24 -0700

    Added message parameters.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    key: 'D63522D9508673C783E8F1EFF20EC27DFB25D64C38353D8C2CD767D109BD15B3'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  key: 'D63522D9508673C783E8F1EFF20EC27DFB25D64C38353D8C2CD767D109BD15B3'
 };
 
 /*
