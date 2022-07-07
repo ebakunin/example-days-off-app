569a0df03f7ace3011f8e3dcdc07b07319e131db
ebakunin <ebakunin@gmail.com>
2022-07-07 14:44:40 -0700

    Added PrimeNG translation functionality.

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { BehaviorSubject, debounceTime, skip, tap } from 'rxjs';
-import { distinctUntilKeyChanged, switchMap } from 'rxjs/operators';
+import { PrimeNGConfig } from 'primeng/api';
+import { BehaviorSubject, skip, tap } from 'rxjs';
+import { delay, distinctUntilKeyChanged, switchMap, take } from 'rxjs/operators';
 
 import { SpinnerService } from './spinner.service';
 import { Language } from '../models/language.model';
@@ -12,7 +13,11 @@ export class LanguageService {
     readonly languages$ = new BehaviorSubject<Language[]>(AVAILABLE_LANGUAGES as Language[]);
     readonly selectedLanguage$ = new BehaviorSubject<Language>(English);
 
-    constructor(private _spinnerService: SpinnerService, private _translateService: TranslateService) {
+    constructor(
+        private _config: PrimeNGConfig,
+        private _spinnerService: SpinnerService,
+        private _translateService: TranslateService
+    ) {
         this.#checkForPreselectedLanguage();
         this.#listenForLanguageChange();
     }
@@ -27,12 +32,16 @@ export class LanguageService {
         );
 
         if (preselectedLanguage) {
+            this._translateService.get('PRIMENG').pipe(take(1)).subscribe((translations) => {
+                this._config.setTranslation(translations);
+            });
             this.selectedLanguage$.next(preselectedLanguage);
         }
     }
 
     /**
      * Listen for a language selection change and update translations throughout the app.
+     * This includes any PrimeNG components.
      * @private
      */
     #listenForLanguageChange(): void {
@@ -42,7 +51,14 @@ export class LanguageService {
             tap((language) => localStorage.setItem('currentLanguage', language.isoCode)),
             tap(() => this._spinnerService.start()),
             switchMap((language) => this._translateService.use(language.isoCode)),
-            debounceTime(500),
+            switchMap(() => this._translateService.get('PRIMENG')),
+            tap((translations) => this._config.setTranslation(translations)),
+            delay(500),
+            tap(() => {
+                // Trigger any PrimeNG calendars to refresh their display
+                const calendars: NodeListOf<HTMLElement> = document.querySelectorAll('.p-datepicker-calendar');
+                calendars?.forEach((calendar) => calendar.click());
+            }),
             tap(() => this._spinnerService.stop())
         ).subscribe();
     }
